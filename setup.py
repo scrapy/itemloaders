cbfe5568b785290b908b9bc9fd98da09a5165858
Júlio César Batista <jcb.1611@gmail.com>
2020-06-18 07:57:06 -0300

    Update setup.py
    Co-authored-by: Adrián Chaves <adrian@chaves.io>

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'w3lib>=1.17.0',
         'parsel>=1.5.0',
         'jmespath>=0.9.5',
-        'itemadapter>=0.1.0'
+        'itemadapter>=0.1.0',
     ],
     # extras_require=extras_require,
 )
