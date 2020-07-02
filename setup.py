c4b07d677cccd5eea93dce89cef75aa1ac676cb1
Júlio César Batista <jcb.1611@gmail.com>
2020-07-02 09:20:21 -0300

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='itemloaders',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/scrapy/itemloaders',
     project_urls={
         'Documentation': 'https://itemloaders.readthedocs.io/',
