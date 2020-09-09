e6c9d3e45a596bb92139857cd11e0b7983ee6417
Adrián Chaves <adrian@chaves.io>
2020-09-09 11:14:46 +0200

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='itemloaders',
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/scrapy/itemloaders',
     project_urls={
         'Documentation': 'https://itemloaders.readthedocs.io/',
