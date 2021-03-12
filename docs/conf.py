e13419c1200ff20e251906d70688ed34aedc8e2c
Adrián Chaves <adrian@chaves.io>
2021-03-12 12:34:51 +0100

    docs/conf.py: use the legal company name for the copyright notice

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'itemloaders'
-copyright = '2020–{}, Zyte'.format(datetime.now().year)
+copyright = '2020–{}, Zyte Group Ltd'.format(datetime.now().year)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
