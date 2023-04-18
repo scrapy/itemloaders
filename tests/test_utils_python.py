0cdb0fdde978d5fe551b742ea19849c13eb11e91
Serhii Abarovskyi <aserhii@protonmail.com>
2023-04-18 14:33:45 +0300

    Replace get_func_args

--- tests/test_utils_python.py
+++ tests/test_utils_python.py
@@ -37,27 +37,26 @@ class UtilsPythonTestCase(unittest.TestCase):
         partial_f2 = functools.partial(f1, b=None)
         partial_f3 = functools.partial(partial_f2, None)
 
-        self.assertEqual(get_func_args(f1), ['a', 'b', 'c'])
-        self.assertEqual(get_func_args(f2), ['a', 'b', 'c'])
-        self.assertEqual(get_func_args(f3), ['a', 'b', 'c'])
-        self.assertEqual(get_func_args(A), ['a', 'b', 'c'])
-        self.assertEqual(get_func_args(a.method), ['a', 'b', 'c'])
-        self.assertEqual(get_func_args(partial_f1), ['b', 'c'])
-        self.assertEqual(get_func_args(partial_f2), ['a', 'c'])
-        self.assertEqual(get_func_args(partial_f3), ['c'])
-        self.assertEqual(get_func_args(cal), ['a', 'b', 'c'])
+        self.assertEqual(get_func_args(f1), ["a", "b", "c"])
+        self.assertEqual(get_func_args(f2), ["a", "b", "c"])
+        self.assertEqual(get_func_args(f3), ["a", "b", "c"])
+        self.assertEqual(get_func_args(A), ["a", "b", "c"])
+        self.assertEqual(get_func_args(a.method), ["a", "b", "c"])
+        self.assertEqual(get_func_args(partial_f1), ["b", "c"])
+        self.assertEqual(get_func_args(partial_f2), ["a", "c"])
+        self.assertEqual(get_func_args(partial_f3), ["c"])
+        self.assertEqual(get_func_args(cal), ["a", "b", "c"])
         self.assertEqual(get_func_args(object), [])
+        self.assertEqual(get_func_args(str.split, stripself=True), ["sep", "maxsplit"])
+        self.assertEqual(get_func_args(" ".join, stripself=True), ["iterable"])
 
-        if platform.python_implementation() == 'CPython':
-            # TODO: how do we fix this to return the actual argument names?
-            self.assertEqual(get_func_args(str.split), [])
-            self.assertEqual(get_func_args(" ".join), [])
+        if platform.python_implementation() == "CPython":
+            # doesn't work on CPython: https://bugs.python.org/issue42785
             self.assertEqual(get_func_args(operator.itemgetter(2)), [])
-        elif platform.python_implementation() == 'PyPy':
-            self.assertEqual(get_func_args(str.split, stripself=True), ['sep', 'maxsplit'])
-            self.assertEqual(get_func_args(operator.itemgetter(2), stripself=True), ['obj'])
-
-            self.assertEqual(get_func_args(" ".join, stripself=True), ['iterable'])
+        elif platform.python_implementation() == "PyPy":
+            self.assertEqual(
+                get_func_args(operator.itemgetter(2), stripself=True), ["obj"]
+            )
 
 
 if __name__ == "__main__":
