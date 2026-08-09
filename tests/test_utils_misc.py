a7e24ad93bd9d9fc76c8ddbc85deed4bf7980701
Adrian Chaves <adrian@zyte.com>
2026-08-09 14:44:46 +0200

    Restore support for any iterable in arg_to_iter

--- tests/test_utils_misc.py
+++ tests/test_utils_misc.py
@@ -1,4 +1,6 @@
-from itemloaders.utils import arg_to_iter
+from collections.abc import Set as AbstractSet
+
+from itemloaders.utils import _MAX_ITERABLE_CLASSES, _iterable_classes, arg_to_iter
 
 
 def test_arg_to_iter():
@@ -20,3 +22,48 @@ def test_arg_to_iter():
     assert list(arg_to_iter({"a": 1}.keys())) == ["a"]
     assert list(arg_to_iter(map(str, [1, 2]))) == ["1", "2"]
     assert list(arg_to_iter(iter([1, 2]))) == [1, 2]
+
+
+def test_arg_to_iter_custom_iterator():
+    class Countdown:
+        def __init__(self, start: int):
+            self.value = start
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if not self.value:
+                raise StopIteration
+            self.value -= 1
+            return self.value
+
+    assert list(arg_to_iter(Countdown(2))) == [1, 0]
+
+
+def test_arg_to_iter_late_registration():
+    class Pair:
+        def __init__(self, first: int, second: int):
+            self._items = (first, second)
+
+        def __iter__(self):
+            return iter(self._items)
+
+        def __len__(self):
+            return 2
+
+        def __contains__(self, value):
+            return value in self._items
+
+    pair = Pair(1, 2)
+    assert list(arg_to_iter(pair)) == [pair]
+
+    AbstractSet.register(Pair)
+    assert list(arg_to_iter(pair)) == [1, 2]
+
+
+def test_arg_to_iter_dynamic_classes():
+    for index in range(_MAX_ITERABLE_CLASSES + 10):
+        obj = type(f"Dynamic{index}", (), {})()
+        assert list(arg_to_iter(obj)) == [obj]
+    assert len(_iterable_classes) <= _MAX_ITERABLE_CLASSES
